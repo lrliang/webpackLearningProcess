a49c1c7f378a7cbf301b7e22a43753f6290c3f80
Liang <392425595@qq.com>
2019-06-29 18:40:56 +0800

    使用面向对象的方式来编写代码控制DOM，特点：网站只需要一个Html，需要加载很多js文件。带来的好处：逻辑更加清晰，debug只需要查看js文件中的逻辑。缺点：需要加载很多js文件，有损用户体验,不知道文件之间的关系，变量都是全局的，难以定位问题

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-var root = document.getElementById('root');
+var dom = document.getElementById('root');
 
-var header = document.createElement('div');
-header.innerHTML = 'Header'
-
-var sideBar = document.createElement('div');
-sideBar.innerHTML = 'SideBar'
-
-var content = document.createElement('div');
-content.innerHTML = 'Content'
-
-root.append(header);
-root.append(sideBar);
-root.append(content);
+new Header()
+new Content()
+new SideBar()
---
+++ src/js/sideBar.js
@@ -0,0 +1,5 @@
+function SideBar(){
+    var sideBar = document.createElement('div');
+    sideBar.innerHTML = 'SideBar'
+    dom.append(sideBar)
+}
