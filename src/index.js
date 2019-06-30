57abda285179be4e76c28e1f70129f6bcf710af1
Liang <392425595@qq.com>
2019-06-30 11:42:44 +0800

    使用file-loader打包图片文件， 打包之后的文件默认会复制到dist目录，并将生成的hash值作为文件名 使用该图片的代码将会获取到该文件的文件名 options.name参数可以用占位符定义打包出来的文件名：name: '[name]_[hash].[ext]' 可以定义打包出来的文件统一存放的位置：outputPath: 'images/'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import Header from './js/header'
 import Content from './js/content'
 import SideBar from './js/sideBar'
+import logo from './logo.jpg'
+
+console.log(logo)
+const img = new Image()
+img.src = logo
+
+const root = document.getElementById('root')
+root.append(img)
 
 new Header()
 new Content()
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,19 @@ const path = require('path')
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.jpg/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name]_[hash].[ext]',
+            outputPath: 'images/'
+          },
+        },
+      }],
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
